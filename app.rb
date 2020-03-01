a82b1aea67d48f09686f1a183d0d215e006b6712
toshi-ue <toshihiro.u.0207@gmail.com>
2020-03-01 18:14:19 +0900

    add: Sinatraでwebアプリ化

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require_relative 'tweet.rb'
+
+# URL'/'でアクセス
+get '/' do
+  'under construction'
+end
+
+# URL'/random_tweet'でアクセス
+get '/tweet_1week_commits' do
+  Tweet.new.tweet_1week_commits
+  'Please check your tweet'
+end
